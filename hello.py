8feb28da842c8da46c57ed164227d293586a95ae
lvhuiyang <ilvhuiyang@gmail.com>
2018-05-22 12:57:37 +0800

    添加依赖 & 项目初始化

---
+++ hello.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# __author__ = lvhuiyang
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+if __name__ == '__main__':
+    app.run()
